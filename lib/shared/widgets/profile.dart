59db0463c32dbab87403e44a6e4a037b58c7338d
veronica <minkyoung2550@gmail.com>
2025-07-03 14:00:39 +0900

    feat: add opacity in profile component(#8)

--- lib/shared/widgets/profile.dart
+++ lib/shared/widgets/profile.dart
@@ -3,25 +3,30 @@ import 'package:flutter/material.dart';
 class Profile extends StatelessWidget {
   final String? profileImageUrl;
   final String defaultImagePath;
+  final double opacity;
 
   const Profile({
     super.key,
     this.profileImageUrl,
     this.defaultImagePath = 'assets/images/default_profile.png',
+    this.opacity = 1.0,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 48,
-      height: 48,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        image: DecorationImage(
-          image: profileImageUrl != null
-              ? NetworkImage(profileImageUrl!) as ImageProvider
-              : AssetImage(defaultImagePath),
-          fit: BoxFit.cover,
+    return Opacity(
+      opacity: opacity,
+      child: Container(
+        width: 48,
+        height: 48,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: profileImageUrl != null
+                ? NetworkImage(profileImageUrl!) as ImageProvider
+                : AssetImage(defaultImagePath),
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
