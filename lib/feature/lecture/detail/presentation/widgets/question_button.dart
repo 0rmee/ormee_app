b3970577b94248db4bb3925d9763638ec4211c80
kimjihyun <kimjihyun@MacBook-Pro.local>
2025-07-07 14:29:28 +0900

    feat: teacher card widget (#12)

---
+++ lib/feature/lecture/detail/presentation/widgets/question_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:ormee_app/shared/theme/app_colors.dart';
+import 'package:ormee_app/shared/theme/app_fonts.dart';
+
+class QuestionButton extends StatelessWidget {
+  const QuestionButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final activeColor = OrmeeColor.gray[20];
+    final pressedColor = OrmeeColor.gray[40];
+    final textColor = OrmeeColor.gray[90];
+
+    return Container(
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () => context.pop(),
+          borderRadius: BorderRadius.circular(12),
+          splashColor: Colors.transparent,
+          highlightColor: pressedColor,
+          child: Ink(
+            padding: EdgeInsets.symmetric(vertical: 7, horizontal: 16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: activeColor,
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [Headline2SemiBold16(text: '질문하기', color: textColor)],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/feature/lecture/detail/presentation/widgets/teacher_card.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:ormee_app/feature/lecture/detail/presentation/widgets/question_button.dart';
+import 'package:ormee_app/shared/theme/app_colors.dart';
+import 'package:ormee_app/shared/theme/app_fonts.dart';
+import 'package:ormee_app/shared/widgets/day_badge.dart';
+import 'package:ormee_app/shared/widgets/profile.dart';
+
+class OrmeeTeacherCard extends StatelessWidget {
+  final List<String> teacherNames; // 선생님 이름 리스트
+  final List<String>? teacherImages; // 선생님 프로필 사진
+  final String startTime; // 강의 시작 시간
+  final String endTime; // 강의 종료 시간
+  final String startPeriod; // 강의 시작 기간
+  final String endPeriod; // 강의 종료 기간
+  final List<String> day; // 요일 리스트
+
+  const OrmeeTeacherCard({
+    super.key,
+    required this.teacherNames,
+    this.teacherImages,
+    required this.startTime,
+    required this.endTime,
+    required this.startPeriod,
+    required this.endPeriod,
+    required this.day,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+      child: Row(
+        children: [
+          Wrap(
+            children: [
+              // 선생님 이미지 있는 경우
+              if (teacherImages != null)
+                teacherImages!.length == 1
+                    ? Profile(profileImageUrl: teacherImages![0])
+                    : MultiProfile(
+                        profileImageUrl: teacherImages![0],
+                        otherProfileImageUrl: teacherImages![1],
+                      ),
+              // 선생님 이미지 없는 경우
+              if (teacherImages == null)
+                teacherNames.length == 1 ? Profile() : MultiProfile(),
+            ],
+          ),
+          SizedBox(width: 12),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Wrap(
+                    children: [
+                      for (int i = 0; i < teacherNames.length; i++) ...[
+                        Headline2SemiBold16(
+                          text: teacherNames[i],
+                          color: OrmeeColor.gray[80],
+                        ),
+                        if (i != teacherNames.length - 1)
+                          Headline2SemiBold16(
+                            text: ' ∙ ',
+                            color: OrmeeColor.gray[80],
+                          ),
+                      ],
+                    ],
+                  ),
+                  SizedBox(width: 2),
+                  Label2Semibold12(text: '선생님', color: OrmeeColor.gray[60]),
+                ],
+              ),
+              SizedBox(height: 8),
+              Row(
+                children: [
+                  Wrap(
+                    children: [
+                      for (int i = 0; i < day.length; i++) ...[
+                        DayBadge(text: day[i]),
+                        if (i != day.length - 1) SizedBox(width: 2),
+                      ],
+                    ],
+                  ),
+                  SizedBox(width: 4),
+                  Label2Regular12(
+                    text: '$startTime - $endTime',
+                    color: OrmeeColor.gray[60],
+                  ),
+                ],
+              ),
+              SizedBox(height: 4),
+              Label2Regular12(
+                text: '$startPeriod - $endPeriod',
+                color: OrmeeColor.gray[50],
+              ),
+            ],
+          ),
+          Spacer(),
+          QuestionButton(),
+        ],
+      ),
+    );
+  }
+}
