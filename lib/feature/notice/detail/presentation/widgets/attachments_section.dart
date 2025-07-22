b9f0cc4335bd0da8550cf418271b5958236fc8aa
민경 <tjalsrud0101@gmail.com>
2025-07-23 01:01:51 +0900

    fix: 오버플로우 에러

--- lib/feature/notice/detail/presentation/widgets/attachments_section.dart
+++ lib/feature/notice/detail/presentation/widgets/attachments_section.dart
@@ -13,20 +13,28 @@ class AttachmentsSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: OrmeeColor.gray[10],
+        borderRadius: BorderRadius.circular(5)
+      ),
       child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Label2Regular12(text: '첨부파일', color: OrmeeColor.gray[50]),
-          const SizedBox(width: 12),
-          Wrap(
-            spacing: 5,
-            runSpacing: 5,
-            children: attachmentFiles.map((file) {
-              return Downloader(fileName: file.name, url: file.url);
-            }).toList(),
+          const SizedBox(width: 8),
+          Expanded(
+            child: Wrap(
+              spacing: 5,
+              runSpacing: 5,
+              children: attachmentFiles.map((file) {
+                return Downloader(fileName: file.name, url: file.url);
+              }).toList(),
+            ),
           ),
         ],
       ),
     );
+
   }
 }
