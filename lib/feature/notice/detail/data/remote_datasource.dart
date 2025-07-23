5f58d206061b56553ebbb2e9e269f2a7e4704b51
민경 <tjalsrud0101@gmail.com>
2025-07-23 21:04:46 +0900

    fix: api interceptor 적용

--- lib/feature/notice/detail/data/remote_datasource.dart
+++ lib/feature/notice/detail/data/remote_datasource.dart
@@ -1,53 +1,45 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:ormee_app/core/constants/api.dart';
-import 'package:ormee_app/feature/auth/token/update.dart';
+import 'package:dio/dio.dart';
 import 'package:ormee_app/feature/notice/detail/data/model.dart';
+import 'package:ormee_app/core/network/api_client.dart';
 
 class NoticeDetailRemoteDataSource {
-  final http.Client client;
-
-  NoticeDetailRemoteDataSource(this.client);
+  final Dio _dio = ApiClient.instance.dio;
 
   Future<NoticeDetailModel> fetchNoticeDetail(int noticeId) async {
-    final accessToken = await AuthStorage.getAccessToken();
+    try {
+      final response = await _dio.get('/students/notices/$noticeId');
 
-    final response = await client.get(
-      Uri.parse('${API.hostConnect}/students/notices/$noticeId'),
-      headers: {'Authorization': 'Bearer $accessToken'},
-    );
-
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      return NoticeDetailModel.fromJson(data);
-    } else {
-      throw Exception('공지를 불러오지 못했습니다.');
+      if (response.statusCode == 200 && response.data != null) {
+        return NoticeDetailModel.fromJson(response.data);
+      } else {
+        throw Exception('공지 데이터를 불러올 수 없습니다.');
+      }
+    } catch (e) {
+      throw Exception('공지 데이터를 불러오는 중 오류가 발생했습니다.');
     }
   }
 
   Future<void> likeNotice(int noticeId) async {
-    final accessToken = await AuthStorage.getAccessToken();
+    try {
+      final response = await _dio.put('/students/notices/$noticeId/like');
 
-    final response = await client.put(
-      Uri.parse('${API.hostConnect}/students/notices/$noticeId/like'),
-      headers: {'Authorization': 'Bearer $accessToken'},
-    );
-
-    if (response.statusCode != 200) {
-      throw Exception('잠시 후 다시 시도해 주세요.');
+      if (response.statusCode != 200) {
+        throw Exception('공지 좋아요에 실패했습니다. 잠시 후 다시 시도해주세요.');
+      }
+    } catch (e) {
+      throw Exception('공지 좋아요 요청 중 오류가 발생했습니다.');
     }
   }
 
   Future<void> unlikeNotice(int noticeId) async {
-    final accessToken = await AuthStorage.getAccessToken();
+    try {
+      final response = await _dio.put('/students/notices/$noticeId/unlike');
 
-    final response = await client.put(
-      Uri.parse('${API.hostConnect}/students/notices/$noticeId/unlike'),
-      headers: {'Authorization': 'Bearer $accessToken'},
-    );
-
-    if (response.statusCode != 200) {
-      throw Exception('잠시 후 다시 시도해 주세요.');
+      if (response.statusCode != 200) {
+        throw Exception('공지 좋아요 취소에 실패했습니다. 잠시 후 다시 시도해주세요.');
+      }
+    } catch (e) {
+      throw Exception('공지 좋아요 취소 요청 중 오류가 발생했습니다.');
     }
   }
 }
