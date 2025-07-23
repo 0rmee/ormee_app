2556ddc63dd1d94e790977879ef8bea54a0bb147
민경 <tjalsrud0101@gmail.com>
2025-07-24 00:49:56 +0900

    feat: 숙제 상세 데이터(#59)

---
+++ lib/feature/homework/detail/data/model.dart
@@ -0,0 +1,72 @@
+import 'package:ormee_app/core/model/file_attachment.dart';
+import 'package:ormee_app/shared/utils/file_utils.dart';
+
+class HomeworkDetailModel {
+  final String title;
+  final String description;
+  final DateTime openTime;
+  final DateTime dueTime;
+  final bool isSubmitted;
+  final bool feedbackCompleted;
+  final AuthorModel author;
+
+  final List<String> imageUrls;
+  final List<AttachmentFile> attachmentFiles;
+
+  HomeworkDetailModel({
+    required this.title,
+    required this.description,
+    required this.openTime,
+    required this.dueTime,
+    required this.isSubmitted,
+    required this.feedbackCompleted,
+    required this.author,
+    required this.imageUrls,
+    required this.attachmentFiles,
+  });
+
+  factory HomeworkDetailModel.fromJson(Map<String, dynamic> json) {
+    final List<dynamic> fileNames = json['data']['fileNames'] ?? [];
+    final List<dynamic> filePaths = json['data']['filePaths'] ?? [];
+
+    List<String> images = [];
+    List<AttachmentFile> files = [];
+
+    for (int i = 0; i < filePaths.length; i++) {
+      final url = filePaths[i] as String;
+      final name = fileNames.length > i ? fileNames[i] as String : '파일';
+
+      if (FileUtil.isImageFile(url)) {
+        images.add(url);
+      } else {
+        files.add(AttachmentFile(name: name, url: url));
+      }
+    }
+
+    return HomeworkDetailModel(
+      title: json['data']['title'] ?? '',
+      description: json['data']['description'] ?? '',
+      openTime: DateTime.parse(json['data']['openTime']),
+      dueTime: DateTime.parse(json['data']['dueTime']),
+      isSubmitted: json['data']['submitted'] ?? false,
+      feedbackCompleted: json['data']['feedbackCompleted'] ?? false,
+      author: AuthorModel.fromJson(
+        json['data']['author'] ?? '',
+        json['data']['authorImage'],
+      ),
+      imageUrls: images,
+      attachmentFiles: files,
+    );
+  }
+}
+
+class AuthorModel {
+  final String name;
+  final String image;
+
+  AuthorModel({required this.name, required this.image});
+
+  factory AuthorModel.fromJson(String name, String image) {
+    return AuthorModel(name: name, image: image);
+  }
+}
---
+++ lib/feature/homework/detail/data/remote_datasource.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:ormee_app/core/network/api_client.dart';
+import 'package:ormee_app/feature/homework/detail/data/model.dart';
+
+class HomeworkDetailRemoteDataSource {
+  final Dio _dio = ApiClient.instance.dio;
+
+  Future<HomeworkDetailModel> fetchHomeworkDetail(int homeworkId) async {
+    try {
+      final response = await _dio.get('/students/homeworks/$homeworkId');
+
+      if (response.statusCode == 200 && response.data != null) {
+        return HomeworkDetailModel.fromJson(response.data);
+      } else {
+        throw Exception('숙제 데이터를 불러올 수 없습니다.');
+      }
+    } catch (e) {
+      throw Exception('숙제 데이터를 불러오는 중 오류가 발생했습니다.');
+    }
+  }
+}
